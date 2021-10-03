e2f674a7a4a14e433afd15f5b108c88809c4e099
Benjamin Sanchez <bjsanchezb@gmail.com>
2021-10-03 13:42:10 -0300

    refactor: create RunMap class
    all functions are now methods within the class RunMap

--- map-runs/map_runs.py
+++ map-runs/map_runs.py
@@ -10,46 +10,61 @@ lat = 55.760274
 lon = 12.541296
 
 
-def add_run(base_map, file_name):
-    """Function that adds a run to the map"""
+class RunMap():
 
-    gpx_file = open(file_name, 'r')
-    gpx = gpxpy.parse(gpx_file)
-    run = list()
-    for track in gpx.tracks:
-        activity = re.split("\d", track.name)[0]
-        for segment in track.segments:
-            for point in segment.points:
-                run.append([point.longitude, point.latitude])
+    def __init__(self):
+        self.Map = folium.Map(
+            location=[lat, lon],
+            tiles='Stamen Terrain',
+            zoom_start=12
+        )
+        print("Successfully initialized map")
 
-    track_name = activity + "/ " + file_name[:10]
-    geojson = folium.GeoJson({'type': 'LineString', 'coordinates': run},
-                             style_function=lambda feature: {'color': '#b80f0a', 'opacity': 0.5, 'weight': 3},
-                             highlight_function=lambda feature: {'color': '#ffc30b', 'opacity': 1, 'weight': 5},
-                             tooltip=track_name)
-    geojson.add_to(base_map)
+    
+    def add_run(self, file_name):
+        """Function that adds a run to the map"""
+
+        gpx_file = open(file_name, 'r')
+        gpx = gpxpy.parse(gpx_file)
+        run = list()
+        for track in gpx.tracks:
+            activity = re.split("\d", track.name)[0]
+            for segment in track.segments:
+                for point in segment.points:
+                    run.append([point.longitude, point.latitude])
+
+        track_name = activity + "/ " + file_name[:10]
+        geojson = folium.GeoJson({'type': 'LineString', 'coordinates': run},
+                                style_function=lambda feature: {'color': '#b80f0a', 'opacity': 0.5, 'weight': 3},
+                                highlight_function=lambda feature: {'color': '#ffc30b', 'opacity': 1, 'weight': 5},
+                                tooltip=track_name)
+        geojson.add_to(self.Map)
+
+
+    def add_all_runs(self, dir_name):
+
+        for file in os.listdir(dir_name):
+            self.add_run(file)
+        print("Successfully added all runs to map")
+
+
+    def export(self, file_name):
+
+        self.Map.save(file_name)
+        print("Successfully exported map")
 
 
 def create_run_map():
     """Function that creates an updated map with all runs"""
 
     # Initialize map:
-    run_map = folium.Map(
-        location=[lat, lon],
-        tiles='Stamen Terrain',
-        zoom_start=12)
-    print("Successfully initialized map")
+    run_map = RunMap()
 
-    # Add all runs in ./gps-data:
-    os.chdir('./gps-data')
-    for file in os.listdir('.'):
-        add_run(run_map, file)
-    os.chdir('./..')
-    print("Successfully added all runs to map")
+    # Add all runs from ./gps-data:
+    run_map.add_all_runs('./gps-data')
 
     # Export map:
-    run_map.save('./output-map.html')
-    print("Successfully exported map")
+    run_map.export('./output-map.html')
 
 
 # Call from shell:
