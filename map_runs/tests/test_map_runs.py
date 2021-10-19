624487ea9ebb4b3d14dca5411e4bcab82803d58b
Benjamin Sanchez <bjsanchezb@gmail.com>
2021-10-19 22:55:30 +0200

    test: check verbosity
    by looking at the capsys fixture from pytest

--- map_runs/tests/test_map_runs.py
+++ map_runs/tests/test_map_runs.py
@@ -18,22 +18,36 @@ def tmp_output_path(tmp_path):
     return os.path.join(tmp_path, "output.html")
 
 
-def assert_run_map_results(tmp_output_path):
-    """Helper function to check things worked."""
+def assert_run_map_results(capsys, tmp_output_path, verbose):
+    """
+    Helper function to check things worked.
+
+    Parameters
+    ==========
+    capsys: fixture
+        Pytest fixture that provides access to all printed output.
+    tmp_output_path: str
+        Temporal output path of the map.
+    verbose: bool
+        Weather any output should be printed or not.
+    """
 
     # Check if file was created in the proper place:
     assert os.path.isfile(tmp_output_path)
     assert not os.path.isfile(os.path.join(repo_path, "output.html"))
 
+    # Check verbosity:
+    assert verbose == (capsys.readouterr().out != "")
 
-def test_create_run_map_default(tmp_output_path):
+
+def test_create_run_map_default(capsys, tmp_output_path):
     """Test function with default parameters."""
 
     create_run_map(output_path=tmp_output_path)
-    assert_run_map_results(tmp_output_path)
+    assert_run_map_results(capsys, tmp_output_path, True)
 
 
-def test_create_run_map_alt_ini(tmp_path, tmp_output_path):
+def test_create_run_map_alt_ini(capsys, tmp_path, tmp_output_path):
     """Test function with a different .ini file."""
 
     # Create new map-runs.ini file from old one:
@@ -47,10 +61,10 @@ def test_create_run_map_alt_ini(tmp_path, tmp_output_path):
         config.write(configfile)
 
     create_run_map(init_file_path=tmp_ini_file)
-    assert_run_map_results(tmp_output_path)
+    assert_run_map_results(capsys, tmp_output_path, True)
 
 
-def test_create_run_map_alt_data(tmp_path, tmp_output_path):
+def test_create_run_map_alt_data(capsys, tmp_path, tmp_output_path):
     """Test function with data in a different path."""
 
     # Create smaller dataset:
@@ -65,4 +79,11 @@ def test_create_run_map_alt_data(tmp_path, tmp_output_path):
             )
 
     create_run_map(data_path=tmp_data_path, output_path=tmp_output_path)
-    assert_run_map_results(tmp_output_path)
+    assert_run_map_results(capsys, tmp_output_path, True)
+
+
+def test_create_run_map_no_verbose(capsys, tmp_output_path):
+    """Test function without printing anything."""
+
+    create_run_map(output_path=tmp_output_path, verbose=False)
+    assert_run_map_results(capsys, tmp_output_path, False)
